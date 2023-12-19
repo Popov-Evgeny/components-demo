cf73d2bcb36126afe4d604ae5efc58386b70039b
Popov Evgenii <ev.popov@elma-bs.ru>
2023-12-19 21:11:06 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import './App.css';
+import Person from "./Person/Person";
+import React, {useState} from "react";
+
+interface Person {
+    name: string;
+    age: number;
+    hobby?: string;
+}
+function App() {
+    const [people, setPeople] = useState<Person[]>([
+        {name: 'Jane', age: 28, hobby: 'Video games!'},
+        {name: 'John', age: 38, hobby: 'ski!'}
+    ]);
+
+    const changeName = () => {
+        setPeople([
+            {name: 'Jane Doe', age: 28},
+            {name: 'John Doe', age: 38}
+        ]);
+    }
+
+    return (
+        <div className="App">
+            <Person
+                name={people[0].name}
+                age={people[0].age}
+            >
+                <strong>{people[0].hobby}</strong>
+            </Person>
+            <Person
+                name={people[1].name}
+                age={people[1].age}
+            >
+                <strong>{people[1].hobby}</strong>
+            </Person>
+            <div>
+                <button onClick={changeName}>Change name</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
